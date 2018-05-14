6256059509954be25d2ac4a9f4846b030f99d80d
Jan Baykara <janbaykara@gmail.com>
2018-05-15 00:38:16 +0100

    Add voting preference fields to form

---
+++ form.js
@@ -0,0 +1,63 @@
+/**
+ * Focus on form when the URL points to it
+ */
+var $form = document.getElementById('pledge-form')
+
+if (getURLParameter('pledge')) {
+  scrollTo($form)()
+  focus($form)()
+}
+
+/**
+ * Focus on click
+ * Defocus on submit / click away
+ */
+document.addEventListener('click', onDocumentClick($form, defocus($form)))
+$form.addEventListener('click', focus($form))
+
+function scrollTo ($form) {
+  return function (e) {
+    var scrollTop = (
+        (
+          (t = document.documentElement) ||
+          (t = document.body.parentNode)
+        ) && typeof t.scrollTop == 'number'
+        ? t
+        : document.body
+      ).scrollTop
+    var diffHeight = Math.max(0, window.innerHeight - height($form))
+    var offsetTop = $form.getBoundingClientRect().top
+    var y = Number((offsetTop + scrollTop - (diffHeight / 2)).toFixed(0))
+    document.documentElement.scrollTop = document.body.scrollTop = y
+  }
+}
+
+function focus ($form) {
+  return function (e) {
+    $form.classList.add('form-active')
+    $form.querySelector('input').focus()
+  }
+}
+
+function defocus ($form) {
+  return function (e) {
+    $form.classList.remove('form-active')
+  }
+}
+
+function onDocumentClick ($form, handler) {
+  return function (e) {
+    if ($form.contains(e.target) || e.target == $form) {
+      return
+    }
+    handler(e)
+  }
+}
+
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
+
+function height ($el) {
+  return ($el.offsetHeight || $el.clientHeight)
+}
