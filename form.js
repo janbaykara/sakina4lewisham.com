73b9a17347740214eba3195c3a66d00b88a46ac8
Jan Baykara <janbaykara@gmail.com>
2018-05-15 08:49:38 +0100

    Minor corrections, defocus on submit

--- form.js
+++ form.js
@@ -1,68 +1,75 @@
-/**
- * Focus on form when the URL points to it
- */
-var $form = document.getElementById('pledge-form')
+// Bootstrap
 
-if (getURLParameter('pledge')) {
-  scrollTo($form)()
-  focus($form)()
-}
+;(function bootstrap() {
+  var $form = document.getElementById("pledge-form")
 
-/**
- * Focus on click
- * Defocus on submit / click away
- */
-document.addEventListener('click', onDocumentClick($form, defocus($form)))
-$form.addEventListener('click', focus($form))
+  if (getURLParameter("pledge")) {
+    scrollTo($form)()
+    focus($form)()
+  }
 
-function scrollTo ($form) {
-  return function (e) {
-    var scrollTop = (
-        (
-          (t = document.documentElement) ||
-          (t = document.body.parentNode)
-        ) && typeof t.scrollTop == 'number'
-        ? t
-        : document.body
-      ).scrollTop
+  document.addEventListener("click", excludeElement($form, defocus($form)))
+  $form.addEventListener("click", focus($form))
+  $form.addEventListener("submit", defocus($form))
+})()
+
+// Modules
+
+function scrollTo($form) {
+  return function(e) {
+    var scrollTop = (((t = document.documentElement) ||
+      (t = document.body.parentNode)) &&
+    typeof t.scrollTop == "number"
+      ? t
+      : document.body
+    ).scrollTop
     var diffHeight = Math.max(0, window.innerHeight - height($form))
     var offsetTop = $form.getBoundingClientRect().top
-    var y = Number((offsetTop + scrollTop - (diffHeight / 2)).toFixed(0))
+    var y = Number((offsetTop + scrollTop - diffHeight / 2).toFixed(0))
     document.documentElement.scrollTop = document.body.scrollTop = y
   }
 }
 
-function focus ($form) {
-  return function (e) {
-    if ($form.classList.contains('form-active')) {
+function focus($form) {
+  return function(e) {
+    console.log("Test")
+    if ($form.classList.contains("form-active")) {
       return
     }
-    $form.querySelector('input').focus()
-    $form.classList.add('form-active')
+    $form.querySelector("input").focus()
+    $form.classList.add("form-active")
   }
 }
 
-function defocus ($form) {
-  return function (e) {
-    if ($form.classList.contains('form-active')) {
-      $form.classList.remove('form-active')
+function defocus($form) {
+  return function(e) {
+    if ($form.classList.contains("form-active")) {
+      $form.classList.remove("form-active")
     }
   }
 }
 
-function onDocumentClick ($form, handler) {
-  return function (e) {
-    if ($form.contains(e.target) || e.target == $form) {
+function excludeElement($mask, handler) {
+  return function(e) {
+    if ($mask.contains(e.target) || e.target == $mask) {
       return
     }
     handler(e)
   }
 }
 
+// Utils
+
 function getURLParameter(name) {
-  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+  return (
+    decodeURIComponent(
+      (new RegExp("[?|&]" + name + "=" + "([^&;]+?)(&|#|;|$)").exec(
+        location.search
+      ) || [null, ""])[1].replace(/\+/g, "%20")
+    ) || null
+  )
 }
 
-function height ($el) {
-  return ($el.offsetHeight || $el.clientHeight)
+function height($el) {
+  return $el.offsetHeight || $el.clientHeight
 }
